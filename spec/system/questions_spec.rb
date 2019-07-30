a20acfd2dd9bbdbd6a0d19ea07f9c2d9c4e7ea34
OGIYAMA Atsuo <a.ogiyama@gmail.com>
2019-07-30 12:50:05 +0000

    Added another test spec.

--- spec/system/questions_spec.rb
+++ spec/system/questions_spec.rb
@@ -5,8 +5,12 @@ describe '質問管理機能', type: :system do
     before do
       # ユーザーAを作成しておく
       user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      # ユーザーBを作成しておく
+      user_b = FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
       # 質問者がユーザーAである質問を作成しておく
       FactoryBot.create(:question, title: '最初の質問', content: '1ゲット!', user: user_a, status: 1)
+      # 質問者がユーザーBである、未公開の質問を作成しておく
+      FactoryBot.create(:question, title: '未公開の質問', content: '工事中..', user: user_b, status: 0)
     end
     
     context 'ユーザーAがログインしているとき' do
@@ -21,9 +25,16 @@ describe '質問管理機能', type: :system do
         visit questions_index_path
       end
     
-      it 'ユーザーAが作成した質問が表示される' do
+      # ユーザーAが作成した質問が表示される
+      it 'displays question published by user_a' do
         # 作成済みの質問の名称が画面上に表示されていることを確認
         expect(page).to have_content '最初の質問'
+      end
+      
+      # 未公開の質問が表示されない
+      it 'does not display unpublished question' do
+        # 未公開の質問が表示されないことを確認
+        expect(page).to have_no_content '未公開の質問'
       end
     end
   end
